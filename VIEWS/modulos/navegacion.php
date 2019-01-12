764336b4680cf8760dc89791578fe90c82b53a51
JonatMac <jonathan15-08@hotmail.com>
2019-01-12 18:04:42 -0500

    Delete navegacion.php

--- VIEWS/modulos/navegacion.php
+++
@@ -1,8 +0,0 @@
-<nav>
-		<ul>
-			<li><a href="index.php">Inicio</a></li>
-			<li><a href="index.php?action=Nosotros">Nosotros</a></li>
-			<li><a href="index.php?action=Servicios">Servicios</a></li>
-			<li><a href="index.php?action=Contactenos">Contáctenos</a></li>
-		</ul>
-</nav>
