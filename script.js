9b4fe3467384955a08c6bc702d7d7b36e77de99d
JonatMac <jonathan15-08@hotmail.com>
2019-01-12 23:24:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ if ('serviceWorker' in navigator) {
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
+
+function go(){
+if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){
+        document.form.submit();
+    }
+    else{
+         alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
+    }
+} 
